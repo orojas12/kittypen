32e49ce3e78e6216ba242873890b852d6e540e2c
oscar <orojas12@outlook.com>
2024-11-28 20:27:37 -0700

    add line drawing algorithm

--- whiteboard-server/src/whiteboard/canvas.ts
+++ whiteboard-server/src/whiteboard/canvas.ts
@@ -1,22 +1,63 @@
-// width * height * 4
-// each pixel in the canvas is represented by four values in the array,
-// each an 8 bit unsigned integer representing a component of rgba
-// data (red, green, blue, or alpha)
-const DEFAULT_CANVAS_DATA_ARRAY_SIZE = 100 * 100 * 4;
-
 /**
- * Data representation of an HTML canvas element
+ * Data representation of an HTML canvas element.
+ *
+   Each pixel in the canvas is represented by four values in an 8-bit
+   unsigned integer array. Each value represents a component of rgba
+   data (red, green, blue, or alpha)
+
+   Example:
+
+   [[255, 0, 0, 1, 0, 255, 0, 1]]
+
+   The example above represents two pixels, one red and one green.
+   The first four values in the array is the first pixel, the next
+   four values is the second pixel.
  */
 export class Canvas {
-  private data = new Uint8ClampedArray(DEFAULT_CANVAS_DATA_ARRAY_SIZE);
+  private width = 100;
+  private height = 100;
+  private data = new Uint8ClampedArray(this.width * this.height * 4);
+
+  constructor(width?: number, height?: number, data?: Uint8ClampedArray) {
+    this.width = width || this.width;
+    this.height = height || this.height;
+    this.data = data?.slice() || this.data;
+  }
 
   drawLine = (
-    start: [number, number],
-    end: [number, number],
+    startX: number,
+    startY: number,
+    endX: number,
+    endY: number,
     rgba?: [number, number, number, number],
-  ): void => {};
+  ): void => {
+    const dx = endX - startX;
+    const dy = endY - startY;
+    const step = Math.max(Math.abs(dx), Math.abs(dy));
+    if (step !== 0) {
+      const stepX = dx / step;
+      const stepY = dy / step;
+      for (let i = 0; i < step + 1; i++) {
+        this.drawPixel(
+          Math.round(startX + i * stepX),
+          Math.round(startY + i * stepY),
+        );
+      }
+    }
+  };
 
-  getData = (): Readonly<Uint8ClampedArray> => {
-    return this.data;
+  drawPixel = (x: number, y: number): void => {
+    const index = (x + y * this.width) * 4;
+    this.data[index] = 0; // red
+    this.data[index + 1] = 0; // green
+    this.data[index + 2] = 0; // blue
+    this.data[index + 3] = 1; // alpha
+  };
+
+  /**
+   * Returns a copy of the underlying Uint8ClampedArray representing canvas data.
+   */
+  getData = (): Uint8ClampedArray => {
+    return this.data.slice();
   };
 }
