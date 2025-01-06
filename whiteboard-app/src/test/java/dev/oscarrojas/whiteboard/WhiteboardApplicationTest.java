77f48070b2bea6d032d0d25a83b9a0aae062d0c1
Oscar Rojas <orojas12@outlook.com>
2025-01-05 17:00:49 -0700

    add test for registering new connections

---
+++ whiteboard-app/src/test/java/dev/oscarrojas/whiteboard/WhiteboardApplicationTest.java
@@ -0,0 +1,41 @@
+package dev.oscarrojas.whiteboard;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import dev.oscarrojas.whiteboard.ws.AppMessage;
+import dev.oscarrojas.whiteboard.ws.WebSocketMessageHandler;
+import dev.oscarrojas.whiteboard.ws.protocol.AppMessageBinaryEncoder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class WhiteboardApplicationTest {
+
+  @Autowired WebSocketMessageHandler messageHandler;
+
+  @Autowired AppSessionService sessionService;
+
+  @Mock WebSocketSession ws;
+
+  @Test
+  void registersNewConnection() {
+    when(ws.getId()).thenReturn("ws1");
+
+    AppMessageBinaryEncoder encoder = new AppMessageBinaryEncoder();
+    BinaryMessage msg =
+        new BinaryMessage(encoder.encode(new AppMessage("channel1", "action1", new byte[] {0})));
+
+    messageHandler.afterConnectionEstablished(ws);
+
+    AppSession session = sessionService.getSessionForConnection(ws.getId()).orElseThrow();
+    assertTrue(session.hasConnection(ws.getId()));
+  }
+}
