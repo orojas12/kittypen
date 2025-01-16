a68603cda8e06327503bca4937b81f9b6eda0282
Oscar Rojas <orojas12@outlook.com>
2025-01-15 20:03:42 -0700

    discard canvas update action if the update message is outdated

--- whiteboard-app/src/main/java/dev/oscarrojas/whiteboard/canvas/Canvas.java
+++ whiteboard-app/src/main/java/dev/oscarrojas/whiteboard/canvas/Canvas.java
@@ -1,57 +1,71 @@
 package dev.oscarrojas.whiteboard.canvas;
 
 import dev.oscarrojas.whiteboard.exception.InvalidInputException;
+
+import java.time.Instant;
 import java.util.UUID;
 
 public class Canvas {
 
-  private final int DEFAULT_WIDTH = 1000;
-  private final int DEFAULT_HEIGHT = 1000;
+    private final int DEFAULT_WIDTH = 1000;
+    private final int DEFAULT_HEIGHT = 1000;
 
-  private String id = UUID.randomUUID().toString();
-  private int width;
-  private int height;
-  private byte[] data;
+    private String id = UUID.randomUUID().toString();
+    private int width;
+    private int height;
+    private byte[] data;
+    private Instant lastUpdated;
 
-  public Canvas() {
-    this.width = DEFAULT_WIDTH;
-    this.height = DEFAULT_HEIGHT;
-  }
-
-  public Canvas(int width, int height) {
-    this.width = width;
-    this.height = height;
-    this.data = new byte[width * height * 4];
-  }
-
-  public Canvas(int width, int height, byte[] data) {
-    this.width = width;
-    this.height = height;
-    this.data = new byte[data.length];
-  }
-
-  String getId() {
-    return id;
-  }
-
-  void setId(String id) {
-    this.id = id;
-  }
-
-  void putData(byte[] array) throws InvalidInputException {
-    if (array.length != width * height * 4) {
-      throw new InvalidInputException("Input array does not match required size");
+    public Canvas() {
+        this.width = DEFAULT_WIDTH;
+        this.height = DEFAULT_HEIGHT;
+        this.lastUpdated = Instant.now();
     }
-    data = array;
-  }
 
-  void reset() {
-    for (int i = 0; i < data.length; i++) {
-      data[i] = 0;
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.data = new byte[width * height * 4];
+        this.lastUpdated = Instant.now();
     }
-  }
 
-  byte[] getData() {
-    return data;
-  }
+    public Canvas(int width, int height, byte[] data) {
+        this.width = width;
+        this.height = height;
+        this.data = new byte[data.length];
+        this.lastUpdated = Instant.now();
+    }
+
+    String getId() {
+        return id;
+    }
+
+    void setId(String id) {
+        this.id = id;
+    }
+
+    void putData(byte[] array) throws InvalidInputException {
+        if (array.length != width * height * 4) {
+            throw new InvalidInputException("Input array does not match required size");
+        }
+        data = array;
+    }
+
+    void reset() {
+        for (int i = 0; i < data.length; i++) {
+            data[i] = 0;
+        }
+    }
+
+    byte[] getData() {
+        return data;
+    }
+
+    public Instant getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Instant lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
 }
--- whiteboard-app/src/main/java/dev/oscarrojas/whiteboard/canvas/CanvasMessageConsumer.java
+++ whiteboard-app/src/main/java/dev/oscarrojas/whiteboard/canvas/CanvasMessageConsumer.java
@@ -7,46 +7,55 @@ import dev.oscarrojas.whiteboard.messaging.AppMessage;
 import dev.oscarrojas.whiteboard.messaging.AppMessageConsumer;
 import dev.oscarrojas.whiteboard.messaging.annotation.Action;
 import dev.oscarrojas.whiteboard.messaging.annotation.Channel;
+import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Optional;
-import org.springframework.stereotype.Component;
 
 @Component
 @Channel("canvas")
 public class CanvasMessageConsumer extends AppMessageConsumer {
 
-  private final AppSessionService sessionService;
+    private final AppSessionService sessionService;
 
-  public CanvasMessageConsumer(AppSessionService sessionService) {
-    this.sessionService = sessionService;
-  }
-
-  @Action("update")
-  private void update(AppMessage message, String connectionId) {
-    Optional<AppSession> sessionOpt = sessionService.getSessionForConnection(connectionId);
-
-    if (sessionOpt.isEmpty()) {
-      return;
+    public CanvasMessageConsumer(AppSessionService sessionService) {
+        this.sessionService = sessionService;
     }
 
-    AppSession session = sessionOpt.get();
+    @Action("update")
+    private void update(AppMessage message, String connectionId) {
+        Optional<AppSession> sessionOpt = sessionService.getSessionForConnection(connectionId);
 
-    try {
-      session.getCanvas().putData(message.getPayload());
-    } catch (InvalidInputException e) {
-      // TODO: figure out how to send error message
-      return;
+        if (sessionOpt.isEmpty()) {
+            return;
+        }
+
+        AppSession session = sessionOpt.get();
+        Canvas canvas = session.getCanvas();
+
+        // update message's timestamp is older than canvas' most recent update
+        if (message.getTimestamp().compareTo(canvas.getLastUpdated()) < 0) {
+            // abort update as this message is outdated and canvas currently
+            // reflects the state from a more recent update message
+            return;
+        }
+
+        try {
+            canvas.putData(message.getPayload());
+        } catch (InvalidInputException e) {
+            // TODO: figure out how to send error message
+            return;
+        }
+
+        sessionService.saveSession(session);
+
+        AppMessage appMessage = new AppMessage("canvas", "update", canvas.getData());
+
+        try {
+            session.broadcastMessage(appMessage, Collections.singletonList(connectionId));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-    sessionService.saveSession(session);
-
-    AppMessage appMessage = new AppMessage("canvas", "update", session.getCanvas().getData());
-
-    try {
-      session.broadcastMessage(appMessage, Collections.singletonList(connectionId));
-    } catch (IOException exc) {
-      throw new RuntimeException(exc);
-    }
-  }
 }
