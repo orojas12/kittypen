773b87eb16dee544b04076d0ed26f560371156bd
Oscar Rojas <orojas12@outlook.com>
2025-01-15 21:03:36 -0700

    add getAllByConnectionCountRange method

--- whiteboard-app/src/main/java/dev/oscarrojas/whiteboard/AppSessionDao.java
+++ whiteboard-app/src/main/java/dev/oscarrojas/whiteboard/AppSessionDao.java
@@ -1,13 +1,22 @@
 package dev.oscarrojas.whiteboard;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AppSessionDao {
 
-  Optional<AppSession> get(String id);
+    Optional<AppSession> get(String id);
 
-  Optional<AppSession> getByConnectionId(String connectionId);
+    Optional<AppSession> getByConnectionId(String connectionId);
 
-  void save(AppSession session);
+    /**
+     * Finds all sessions with a number of connections within a specified range (inclusive).
+     *
+     * @param min minimum number of connections
+     * @param max maximum number of connections
+     */
+    List<AppSession> getAllByConnectionCountRange(int min, int max);
+
+    void save(AppSession session);
 
 }
--- whiteboard-app/src/main/java/dev/oscarrojas/whiteboard/InMemoryAppSessionDao.java
+++ whiteboard-app/src/main/java/dev/oscarrojas/whiteboard/InMemoryAppSessionDao.java
@@ -1,37 +1,47 @@
 package dev.oscarrojas.whiteboard;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public class InMemoryAppSessionDao implements AppSessionDao {
 
-  private Map<String, AppSession> sessions = new ConcurrentHashMap<>();
+    private Map<String, AppSession> sessions = new ConcurrentHashMap<>();
 
-  @Override
-  public Optional<AppSession> get(String id) {
-    return Optional.ofNullable(sessions.get(id));
-  }
-
-  @Override
-  public Optional<AppSession> getByConnectionId(String connectionId) {
-    AppSession session = null;
-
-    for (AppSession sessionEntry : sessions.values()) {
-      if (sessionEntry.hasConnection(connectionId)) {
-        session = sessionEntry;
-        break;
-      }
+    @Override
+    public Optional<AppSession> get(String id) {
+        return Optional.ofNullable(sessions.get(id));
     }
 
-    return Optional.ofNullable(session);
-  }
+    @Override
+    public Optional<AppSession> getByConnectionId(String connectionId) {
+        AppSession session = null;
 
-  @Override
-  public void save(AppSession session) {
-    assert session.getId() != null;
-    sessions.put(session.getId(), session);
-  }
+        for (AppSession sessionEntry : sessions.values()) {
+            if (sessionEntry.hasConnection(connectionId)) {
+                session = sessionEntry;
+                break;
+            }
+        }
+
+        return Optional.ofNullable(session);
+    }
+
+    @Override
+    public List<AppSession> getAllByConnectionCountRange(int min, int max) {
+        return sessions.values().stream().filter((session) -> {
+            int connections = session.getConnectionCount();
+            return connections >= min && connections <= max;
+        }).toList();
+    }
+
+    @Override
+    public void save(AppSession session) {
+        assert session.getId() != null;
+        sessions.put(session.getId(), session);
+    }
 }
