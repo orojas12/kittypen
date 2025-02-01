ae907438013ae4ae7f9058781c1b6b6f317dbb6c
Oscar Rojas <orojas12@outlook.com>
2025-02-01 16:17:10 -0700

    add canvas wrapper styling

--- whiteboard-client/src/main.ts
+++ whiteboard-client/src/main.ts
@@ -1,14 +1,18 @@
 import "./style.css";
 import { Canvas } from "./canvas/canvas";
 
-const canvasElement = document.getElementById("canvas") as HTMLCanvasElement;
+const canvasElement1 = document.getElementById("canvas1") as HTMLCanvasElement;
 
-if (canvasElement === null) {
+if (canvasElement1 === null) {
   throw Error("Canvas is null");
 }
 
-const ctx = canvasElement.getContext("2d") as CanvasRenderingContext2D;
+const ctx1 = canvasElement1.getContext("2d") as CanvasRenderingContext2D;
 
-const canvas = new Canvas(ctx, { width: 1000, height: 1000, lineWidth: 1 });
+const canvas1 = new Canvas(ctx1, {
+  width: 1000,
+  height: 1000,
+  lineWidth: 4,
+});
 
-const resetBtn = document.getElementById("reset") as HTMLButtonElement;
+// const resetBtn = document.getElementById("reset") as HTMLButtonElement;
